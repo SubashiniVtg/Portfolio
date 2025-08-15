f68bee5c391ba34e2a1adbb51cad052974ad1678
913122104161_SUBASHINI <22cseb28subashini@gmail.com>
2025-08-15 14:44:00 +0530

    Fix 404 redirect and base path

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,21 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, "", redirect);
+}
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename="/Portfolio">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
